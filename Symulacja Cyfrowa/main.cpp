09aa2c0bd66b4a323703ff95de028969f81b64cb
Maks <maksymilianstanko>
2022-05-12 22:42:30 +0200

    spdlog added

--- Symulacja Cyfrowa/main.cpp
+++ Symulacja Cyfrowa/main.cpp
@@ -1,10 +1,12 @@
 ﻿#include <iostream>
 #include "simulator.h"
+#include <spdlog/spdlog.h>
 
 using namespace std;
 
 int main()
 {
+	spdlog::info("Welcome to spdlog!");
 	Simulator simulation(10, 5, 20, 5, 200);
 	simulation.RunSimulation(100000, false);
 	return 0;
