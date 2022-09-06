964d9fa50bc43025eda95051dadbf1bd1d36f2e6
stankom <maksymilian.stanko@stazysta.comarch.pl>
2022-09-06 13:34:23 +0200

    saving to file

--- Symulacja Cyfrowa/main.cpp
+++ Symulacja Cyfrowa/main.cpp
@@ -1,12 +1,14 @@
 ﻿#include <iostream>
 #include "simulator.h"
 #include <spdlog/spdlog.h>
-#include <chrono>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
+	std::ofstream output_file;
+	output_file.open("E_A.txt", std::ios::app);
 	int mode;
 	double lambda = 0.001, E = 0, last_E = 0;
 	std::cout << "Wybierz tryb:\n1) Tryb ciagly.\n2) Tryb krokowy (Debug)." << std::endl;
@@ -16,9 +18,10 @@ int main()
 		last_E = E;
 		E = simulation.RunSimulation(100000, mode, lambda);
 		lambda += 0.000001;
+		output_file << lambda << " " << E << "\n";
 	} while (E < 0.05);
 
 	spdlog::info("E=" + std::to_string(last_E) + " lambda = " + std::to_string(lambda) + "\n");
-
+	output_file.close();
 	return 0;
 }
