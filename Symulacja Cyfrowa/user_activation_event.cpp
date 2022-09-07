edc885c4991e93555e3a1c09d647a190a7f71eb8
stankom <maksymilian.stanko@stazysta.comarch.pl>
2022-09-07 10:25:56 +0200

    still testing

--- Symulacja Cyfrowa/user_activation_event.cpp
+++ Symulacja Cyfrowa/user_activation_event.cpp
@@ -38,6 +38,7 @@ void UserActivationEvent::Execute() {
 	network_->BandwidthPrint();
 	network_->BufferPrint();
 	// plan next user activation event
+	std::cout << network_->GetLambda() << std::endl;
 	int event_t = user_time_generator_.Exponential(network_->GetLambda()) + network_->clock_;
 	Event* next_request_event = new UserActivationEvent(event_t, network_, event_list_);
 	event_list_->insert(next_request_event);
