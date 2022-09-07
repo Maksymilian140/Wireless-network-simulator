e44f94b09223e41e9f1eec709dd3399ab3192f50
stankom <maksymilian.stanko@stazysta.comarch.pl>
2022-09-07 09:20:55 +0200

    testing

--- Symulacja Cyfrowa/main.cpp
+++ Symulacja Cyfrowa/main.cpp
@@ -9,19 +9,19 @@ int main()
 {
 	std::ofstream output_file;
 	output_file.open("E_A0.txt", std::ios::app);
-	int mode, intensity = 1000;
+	int mode, mean = 150;
 	double lambda, E = 0, last_E = 0;
 	std::cout << "Wybierz tryb:\n1) Tryb ciagly.\n2) Tryb krokowy (Debug)." << std::endl;
 	std::cin >> mode;
 	Simulator simulation(10, 5, 20, 5, 200);
 	do {
-		lambda = static_cast<double>(1) / intensity;
+		lambda = static_cast<double>(1) / mean;
 		last_E = E;
 		E = simulation.RunSimulation(100000, mode, lambda);
-		intensity++;
-		output_file << intensity << " " << E << "\n";
+		output_file << static_cast<double>(mean) / 1000 << " " << E << "\n";
+		mean-=25;
 	} while (E < 0.05);
-	spdlog::info("E=" + std::to_string(last_E) + " lambda = " + std::to_string(lambda) + "\n");
+	spdlog::info("E=" + std::to_string(last_E) + " lambda = " + std::to_string(static_cast<double>(mean) / 1000) + "\n");
 	output_file.close();
 	return 0;
 }
