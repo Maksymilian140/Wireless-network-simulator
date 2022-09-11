7d8b077a89672b604c6bca11ebb34eaa0733ead7
Maksymilian Stańko <maksymilian.stanko@student.put.poznan.pl>
2022-09-11 18:54:45 +0200

    add comment for steering

--- Symulacja Cyfrowa/main.cpp
+++ Symulacja Cyfrowa/main.cpp
@@ -9,7 +9,7 @@ int main()
 {
 	int mode;
 	std::cout << "Wybierz tryb:\n1) Tryb ciagly.\n2) Tryb krokowy (Debug)." << std::endl;
-	std::cin >> mode;
+	std::cin >> mode;		// in debug mode press 'enter' key to continue simulation
 	Simulator simulation(10, 5, 20, 5, 200);
 	simulation.RunSimulation(100000, mode);
 	return 0;
